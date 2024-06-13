aa3fdaa215e08c73b7862e7d5f09126802f76fe9
ashutosh <ashutoshh.dev@gmail.com>
2024-06-14 00:11:24 +0530

    c lang basics

---
+++ 01. Introduction to c language/first-program.c
@@ -0,0 +1,19 @@
+// Write a Program to print the given pattern.
+// - - - - - - - -
+// |             |
+// A             |
+// B             |
+// C             |
+// |             |
+// - - - - - - - -
+#include "stdio.h"
+int main()
+{
+    printf("---------\n");
+    printf("|\t|\n");
+    printf("A\t|\n");
+    printf("B\t|\n");
+    printf("C\t|\n");
+    printf("|\t|\n");
+    printf("---------\n");
+}
