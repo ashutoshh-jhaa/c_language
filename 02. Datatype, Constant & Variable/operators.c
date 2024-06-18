23859fea0bc5e5f0a1fce0386675c1103c93fba8
ashutosh <ashutoshh.dev@gmail.com>
2024-06-18 10:51:05 +0530

    basic programs

---
+++ 02. Datatype, Constant & Variable/operators.c
@@ -0,0 +1,16 @@
+// Write a Program to perform all arithmetic operators such as +, -, *, / and %
+#include "stdio.h"
+
+int main()
+{
+    int a;
+    int b;
+    a = 36;
+    b = 5;
+
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b);
+    printf("a * b = %d\n", a * b);
+}
