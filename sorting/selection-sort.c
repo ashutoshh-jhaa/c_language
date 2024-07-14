b719ea75e756a38aefd96a666d3828b30f1b7ec3
ashutosh <ashutoshh.dev@gmail.com>
2024-07-14 22:03:25 +0530

    selection sort algorithm

---
+++ sorting/selection-sort.c
@@ -0,0 +1,48 @@
+#include "stdio.h"
+void selSort(int *a, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    int minIndex = i;
+    for (int j = i + 1; j < n; j++)
+    {
+      if ((*(a + j)) < (*(a + i)))
+      {
+        minIndex = j;
+      }
+    }
+    if (minIndex != i)
+    {
+      int temp = *(a + minIndex);
+      *(a + minIndex) = *(a + i);
+      *(a + i) = temp;
+    }
+  }
+}
+void insertValues(int *a, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    printf("a[%d] = ", i);
+    scanf("%d", (a + i));
+  }
+}
+void dispArray(int *a, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    printf("a[%d] = %d\n", i, *(a + i));
+  }
+}
+int main()
+{
+  int n; // for the size of array
+  printf("Enter the size of array : ");
+  scanf("%d", &n);
+
+  int a[n]; // declared array of size n
+  insertValues(a, n);
+  selSort(a, n);
+  printf("The sorted array : \n");
+  dispArray(a, n);
+}
