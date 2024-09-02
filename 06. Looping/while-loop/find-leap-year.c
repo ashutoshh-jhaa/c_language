31a152928d6fe5447f55946290082327691636b7
ashutosh <ashutoshh.dev@gmail.com>
2024-09-02 18:32:25 +0530

    leap year calculation

--- 06. Looping/while-loop/find-leap-year.c
+++ 06. Looping/while-loop/find-leap-year.c
@@ -1,19 +1,34 @@
-// Write a Program to print leap years between two given numbers using a while loop.
+// Write a Program to print leap years between two given numbers using a while
+// loop.
+// some century year which are divisible by 4 but are not leap year thus
+// we need to check their divisibilty with 400
 #include "stdio.h"
-int main()
-{
-    int start, end;
-    printf("Enter start year : ");
-    scanf("%d", &start);
-    printf("Enter end year : ");
-    scanf("%d", &end);
+int main() {
+  int start, end;
+  printf("Enter start year : ");
+  scanf("%d", &start);
+  printf("Enter end year : ");
+  scanf("%d", &end);
 
-    while (start <= end)
-    {
-        if (start % 4 == 0)
-        {
-            printf("%d\n", start);
-        }
-        start++;
+  while (start <= end) {
+    // without separating both concerns
+    if ((start % 400 == 0) || (start % 4 == 0 && start % 100 != 0)) {
+      printf("Leap year : %d\n", start);
     }
+    start++;
+
+    // for separating both concerns
+    // if (start % 100 == 0) {
+    //   // For century years
+    //   if (start % 400 == 0) {
+    //     printf("Century leap years : %d\n", start);
+    //   }
+    // } else {
+    //   // For non-century years
+    //   if (start % 4 == 0) {
+    //     printf("Leap year : %d\n", start);
+    //   }
+    // }
+    // start++;
+  }
 }
