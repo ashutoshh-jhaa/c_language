3c55c4136942d95c60c8b26b1d4fff873d2af931
ash <ashutoshh.dev@gmail.com>
2024-12-12 15:37:35 +0530

    n even numbers

--- 13.Recursion/n-even-numbers.c
+++ 13.Recursion/n-even-numbers.c
@@ -10,11 +10,20 @@ void recurEven(int n) {
     }
   }
 }
-
+void recurEven1(int n) {
+  if (n == 0) {
+    return;
+  } else {
+    recurEven1(n - 1);
+    printf("%d\n", 2 * n );
+  }
+}
 int main() {
   int num;
   printf("Enter the value of n : \n");
   scanf("%d", &num);
-  printf("n = %d\n", n);
+  printf("n = %d\n", num);
   recurEven(num);
+  printf("\n");
+  recurEven1(num);
 }
