6773fe5316d022cd6e79c07478cf9a27764ce5f0
ash <ashutoshh.dev@gmail.com>
2024-12-31 22:34:42 +0530

    basics of structure

--- 12. Structure/basics.c
+++ 12. Structure/basics.c
@@ -1,9 +1,7 @@
 #include "stdio.h"
 #include "string.h"
-int main()
-{
-  struct Books
-  {
+int main() {
+  struct Books {
     int bookid;
     float bookprice;
     char bookname[20];
@@ -11,7 +9,7 @@ int main()
 
   b1.bookid = 32;
   b1.bookprice = 23.2f;
-  strcpy(b1.bookname,"ashu");
-  fgets(b1.bookname,10,stdin);
+  strcpy(b1.bookname, "ashu");
+  fgets(b1.bookname, 10, stdin);
   printf("%s", b1.bookname);
 }
