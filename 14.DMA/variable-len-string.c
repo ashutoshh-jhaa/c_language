dd94f8141575a3e8130622768a35e708b67f5806
ash <ashutoshh.dev@gmail.com>
2025-01-02 21:20:54 +0530

    string with dynamic len

---
+++ 14.DMA/variable-len-string.c
@@ -0,0 +1,19 @@
+// string with dynamic length
+#include "stdio.h"
+#include "stdlib.h"
+int main() {
+  char *str, c;
+  str = (char*)malloc(sizeof(char));
+  int i = 0, j = 1;
+
+  while (c != '\n') {
+    c = getc(stdin);
+    j++;
+    str = (char*)realloc(str, j * sizeof(char));
+    str[i] = c;
+    i++;
+  }
+  str[i] = '\0';
+
+  printf("%s", str);
+}
